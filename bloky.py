5285afca1c463f4ab198e322cf9e8c2f33e440d0
JakubJansky973 <janskyjakub1@gmail.com>
2026-06-01 20:55:25 +0200

    Přidána třída pro kostky a jejich pohyb

---
+++ bloky.py
@@ -0,0 +1,32 @@
+import turtle
+
+class bloky:
+    def __init__(self, x, y):
+        self.shape = turtle.Turtle()
+        self.shape.shape("square")
+        self.shape.color("red")
+        self.shape.penup()
+        self.shape.shapesize(stretch_wid=1.5, stretch_len=4)
+        self.shape.goto(x, y)
+        
+        self.block_width = 80
+        self.block_height = 30
+
+class MovingBlock(bloky):
+    def __init__(self, x, y, speed):
+        super().__init__(x, y)
+        self.speed = speed 
+        self.direction = 1
+        
+    def pohybzestranynastranu(self, sirka_obrazovky):
+        new_x = self.shape.xcor() + (self.speed * self.direction)
+        
+        right_edge = (sirka_obrazovky / 2) - (self.block_width / 2)
+        left_edge = -(sirka_obrazovky / 2) + (self.block_width / 2)
+        
+        if new_x > right_edge:
+            self.direction = -1
+        elif new_x < left_edge:
+            self.direction = 1
+            
+        self.shape.setx(new_x)
